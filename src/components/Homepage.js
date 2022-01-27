8bd21736195fa9266deea6b7e22037816eb78a4b
Mohit Arora <mohit.arora@in.ibm.com>
2022-01-27 20:47:44 +0530

    Header Image

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,7 +3,7 @@ import { HomepageBanner, HomepageCallout } from 'gatsby-theme-carbon';
 import HomepageTemplate from 'gatsby-theme-carbon/src/templates/Homepage';
 import { calloutLink } from './Homepage.module.scss';
 
-import Carbon from './carbon.jpg';
+import Carbon from './carbon2.jpg';
 
 const FirstLeftText = () => <p>IBM Cloud Service</p>;
 
